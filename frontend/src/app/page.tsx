ef1252cd3c7d46db87bb16ce659a5b5219848ed7
Stefano Hepburn <stefanohepburn@gmail.com>
2024-07-09 21:21:32 -0400

    add frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			happy birthday
+		</main>
+	);
+}
+
+
